d2609d174f6c5804ece1a44465fa1eca01a38738
blankwriter <prince.eworho@amalitech.org>
2022-03-18 20:06:06 +0000

    left content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./Components/Navbar";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Education from "./Pages/Education/Education";
+import Experience from "./Pages/Experience/Experience";
+import Profile from "./Pages/Profile/Profile";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/education" element={<Education/>}/>
+      <Route path="/experience" element={<Experience/>}/>
+      <Route path="/profile" element={<Profile/>}/>
     </Routes>
     </Router>
     
