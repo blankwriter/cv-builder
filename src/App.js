f2e867a6fcc89518e9ac18fc59b252a3eb789645
blankwriter <prince.eworho@amalitech.org>
2022-03-21 12:02:11 +0000

    structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from "./Components/Navbar";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Education from "./Pages/Education/Education";
-import Experience from "./Pages/Experience/Experience";
-import Profile from "./Pages/Profile/Profile";
+import Education from "./Pages/Education";
+import Experience from "./Pages/Experience";
+import Profile from './Pages/Profile';
 
 function App() {
   return (
